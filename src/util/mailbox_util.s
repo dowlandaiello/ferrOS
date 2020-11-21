ea4e2bad8e8dc7f5612f50ab4b11afffbbdc76c7
Dowland Aiello <dowlandaiello@gmail.com>
2020-11-21 18:47:35 -0500

    util: Edit mailbox_util.s.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/util/mailbox_util.s
+++ src/util/mailbox_util.s
@@ -13,6 +13,7 @@
 
 # Available mailbox operations and their corresponding memory sectors
 .equ	MAILBOX_BASE,   0x2000B880
+.equ	MAILBOX_READ,   MAILBOX_BASE
 .equ	MAILBOX_STATUS, #(MAILBOX_BASE + 0x18)
 .equ	MAILBOX_WRITE,  #(MAILBOX_BASE + 0x20)
 
@@ -26,6 +27,7 @@
 
 .text
 
+/* Blocks the thread until mailbox0 is ready to write */
 block_until_mailbox_writable:
 	push	{r4}
 
@@ -40,6 +42,8 @@ block_until_mailbox_writable:
 	pop	{r4} 
 	ret
 
+/* Writes the contents of register zero to the mailbox0 channel given in
+	register 1 */
 write_mailbox:
 	bl	block_until_mailbox_writable
 
@@ -51,3 +55,46 @@ write_mailbox:
 
 	# Put value specified in r0 in the mailbox write sector
 	str	r0, [r4]
+
+	ret
+
+/* Reads the contents of the mailbox0 channel given in register 0, outputting
+	the read value in register 0 */
+read_mailbox:
+	bl	block_until_mailbox_readable
+
+	# Read until value in mailbox0 read contains least significant 4 bits
+	# that match the channel in r0
+	mov	r1, #-1
+	bl	read_mailbox_helper
+
+	ret
+
+/* Blocks the thread until mailbox0 has a value in it, as indicated by its stat
+	channel */
+block_until_mailbox_readable:
+	ldr	r4, =MAILBOX_STATUS
+	ldr	r4, [r4]
+
+	# Wait for status to not be "EMPTY"
+	and	r4, EMPTY
+	cmp	r4, #0
+	bne	block_until_mailbox_readable
+
+	ret
+
+read_mailbox_helper:
+	# We'll be reading from the READ segment of the mailbox
+	ldr	r1, =MAILBOX_READ
+
+	# Ensure that the response is from the proper channel (channel in r0)
+	ldr	r1, [r1]
+	and	r1, r1, 0xF
+
+	# Channels must be equal to ret
+	cmp	r1, r0
+	bne	read_mailbox_helper
+
+	# Results from r1 in r0, ret
+	mov	r1, r0
+	ret

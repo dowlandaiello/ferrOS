ea019588bb7defc67d7b1968c0c07ca78c534b2b
Dowland Aiello <dowlandaiello@gmail.com>
2020-12-03 21:55:17 -0500

    main: Use new vgat driver.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{panic::PanicInfo, fmt::Write};
+use ferr_os::drivers::io::vgat_out::VgatOut;
 
 const MSG: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vid_buf = 0xb8000 as *mut u8;
-
-    for (i, &byte) in MSG.iter().enumerate() {
-        unsafe {
-            *vid_buf.offset(i as isize * 2) = byte;
-            *vid_buf.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    let mut stdout = VgatOut::default();
+    stdout.write_str("Hello, world!").expect("failed to write to stdout");
 
     loop {} 
 }
